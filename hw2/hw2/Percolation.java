d597bc79acbfb112e40564fe5f8fba430756df2c
Richard Zhou <richard.chow65@gmail.com>
2020-05-25 19:26:09 -0700

    hw2 test

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,70 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class Percolation {
+public class Percolation extends WeightedQuickUnionUF{
+    private final boolean[] x;
+    private final int N;
+    private int open_count;
 
+    // Constructor: Create empty N x N array
+    public Percolation(int N) {
+        super(N * N);
+        this.N = N;
+        x = new boolean[N * N];
+        open_count = 0;
+    }
+
+    // Convert two-d array into one-d
+    private int xytox(int row, int col) {
+        return N * row + col;
+    }
+
+    public void open(int row, int col) {
+        if (isOpen(row, col)) {
+            return;
+        }
+        int id = xytox(row, col);
+        int[] neighbor = {id - 1, id + 1, id - N, id + N};
+        for (int item: neighbor)
+            if (item < N * N && item > 0) {
+                if (isOpen(item)) {
+                    union(item, id);
+                }
+            }
+        x[id] = true;
+        open_count += 1;
+    }
+
+    boolean isOpen(int id) {
+        return x[id];
+    }
+
+    public boolean isOpen(int row, int col) {
+        int id = xytox(row, col);
+        return x[id];
+    }
+
+    public int numberOfOpenSites() {
+        return open_count;
+    }
+
+    public boolean isFull(int row, int col) {
+        for (int i = 0; i < N; i ++) {
+            if (isOpen(row, col)) {
+                if (connected(xytox(row, col), i)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean percolates() {
+        for (int i = 0; i < N; i ++) {
+            if (isFull(N - 1, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,51 @@
 package hw2;
+import java.util.Random;
 
 public class PercolationStats {
+    private final int[] x;
+    private int sum;
+    private final int T;
 
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        x = new int[N * N];
+        sum = 0;
+        this.T = T;
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("N or T should not be less than 0!");
+        }
+        Percolation a = pf.make(N);
+        for (int i = 0; i < T; i ++) {
+            while (!a.percolates()) {
+                Random rand = new Random();
+                int randID = rand.nextInt(N * N);
+                if (!a.isOpen(randID)) {
+                    int row = randID / 5;
+                    int col = randID % 5;
+                    a.open(row, col);
+                }
+            }
+            x[i] = a.numberOfOpenSites();
+            sum += a.numberOfOpenSites();
+        }
+    }
+
+    public double mean() {
+        return (double) sum / T;
+    }
+
+    public double stddev() {
+        int sum2 = 0;
+        for (int item: x) {
+            sum += (item - mean()) * (item - mean());
+        }
+        return  (double) sum / (T - 1);
+    }
+
+    public double confidenceLow() {
+        return mean() - 1.96 * Math.sqrt(stddev()) / Math.sqrt(T);
+    }
+
+    public double confidenceHigh() {
+        return mean() + 1.96 * Math.sqrt(stddev()) / Math.sqrt(T);
+    }
 }
